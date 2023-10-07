19d83ea709e9410dcdc5edbdea8287f54475740c
Artsiom Sashcheka <sashheka@gmail.com>
2023-10-07 23:04:36 +0300

    just a commit, no special

--- header_files/Log.cpp
+++ header_files/Log.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Common.h"
 #include "Log.h"
 
 void InitLog() {
--- header_files/Log.h
+++ header_files/Log.h
@@ -1,4 +1,9 @@
-#pragma once
+#ifndef _LOG_H
+#define _LOG_H
 
 void InitLog();
 void Log(const char* mes);
+
+struct Player {};
+
+#endif
