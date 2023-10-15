5459151a47fa7c5dd1863729fea55eeaecbd515b
sashh <sashheka@gmail.com>
2023-10-15 17:57:31 +0300

    improved Log class with enum

--- classes/main.cpp
+++ classes/main.cpp
@@ -3,32 +3,33 @@
 class Log
 {
 public:
-  const int LogLevelError = 0;
-  const int LogLevelWarning = 1;
-  const int LogLevelInfo = 2;
+  enum Level
+  {
+    LevelError = 0, LevelWarning, LevelInfo
+  };
 private:
-  int m_LogLevel = LogLevelInfo;
+  Level m_LogLevel = LevelInfo;
 public:
-  void SetLevel(int level)
+  void SetLevel(Level level)
   {
     m_LogLevel = level;
   }
   
   void Error(const char* message)
   {
-    if (m_LogLevel >= LogLevelError)
+    if (m_LogLevel >= LevelError)
       std::cout << "[ERROR]: " << message << std::endl;
   }
   
   void Warn(const char* message)
   {
-    if (m_LogLevel >= LogLevelWarning)
+    if (m_LogLevel >= LevelWarning)
       std::cout << "[WARNING]: " << message << std::endl;
   }
   
   void Info(const char* message)
   {
-    if (m_LogLevel >= LogLevelInfo)
+    if (m_LogLevel >= LevelInfo)
       std::cout << "[INFO]: " << message << std::endl;
   }
 };
@@ -36,9 +37,9 @@ public:
 int main()
 {
   Log log;
-  log.SetLevel(log.LogLevelInfo);
-  log.Error("hello");
+  log.SetLevel(Log::LevelError);
   log.Warn("hello");
+  log.Error("hello");
   log.Info("hello");
   std::cin.get();
 }
---
+++ enums/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+enum Example : char {
+  A, B, C
+};
+
+int a = 0;
+int b = 1;
+int c = 2;
+
+int main() {
+  Example value = B;
+}
