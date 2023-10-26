4cca29072b5c610315ddb390668e72f9a5ee19bc
sashh <sashheka@gmail.com>
2023-10-26 21:28:04 +0300

    pure virtual functions

--- cherno_lessons/virtual_functions/main.cpp
+++ cherno_lessons/virtual_functions/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 
-class Entity {
+class Printable {
+  public:
+    virtual std::string GetClassName() = 0;
+};
+
+class Entity : public Printable {
   public:
     virtual std::string GetName() { return "Entity"; }
+    std::string GetClassName() override { return "Entity"; }
 };
 
 class Player : public Entity {
@@ -14,18 +20,26 @@ class Player : public Entity {
       : m_Name(name) {}
       
     std::string GetName() override { return m_Name; }
+    std::string GetClassName() override { return "Player"; }
 };
 
 void PrintName(Entity* entity) {
   std::cout << entity->GetName() << std::endl;
 }
 
+void Print(Printable* obj) {
+  std::cout << obj->GetClassName() << std::endl;
+}
+
 int main() {
   Entity* e = new Entity();
-  PrintName(e);
+//  PrintName(e);
   
   Player* p = new Player("Artsiom");
-  PrintName(p);
+//  PrintName(p);
+
+  Print(e);
+  Print(p);
   
   std::cin.get();
 }
