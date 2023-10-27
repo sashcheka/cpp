45578b727b5255d3f6b8b0e046e492766084cc89
sashh <sashheka@gmail.com>
2023-10-27 16:06:04 +0300

    feat: add Wallet class with createTransaction method

--- simple_blockchain/main.cpp
+++ simple_blockchain/main.cpp
@@ -1,28 +1,78 @@
+#include <ctime>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+class Transaction {
+  public:
+    string senderAddress;
+    string receiverAddress;
+    double amount;
+    long timestamp;
+    string signature;
+  public:
+    void printTransaction() {
+      cout << "Transaction sender address: " << this->senderAddress << "\n";
+      cout << "Transaction receiver address: " << this->receiverAddress << "\n";
+      cout << "Transaction amount: " << this->amount << "\n";
+      cout << "Transaction timestamp: " << this->timestamp << "\n";
+    }
+};
+
 class Block {
   private:
-    int header;
+    int index;
     long timestamp;
     int nonce;
-    int difficulty;
     string hash;
     string previousHash;
   public:
-    
-};
-
-class Transaction {
-  
+    string calculateHash(int& index, long& timestamp, string previousHash) {};
+    int mineBlock(int& difficulty) {};
+    void printBlock() {};
 };
 
 class Blockchain {
-  
+  private:
+    vector<Block> chain;
+    int difficulty;
+  public:
+    Block createGenesisBlock() {};
+    Block addBlock(Block& newBlock) {};
+    bool isValid() {};
+    void printChain() {};
+};
+
+class Wallet {
+  private:
+    string address;
+    string privateKey;
+    string publicKey;
+  public:
+    string generateKeys() {};
+    Transaction createTransaction(string& receiverAddress, double& amount) {
+      long timestamp = time(0);
+
+      Transaction newTransaction;
+      newTransaction.senderAddress = this->address;
+      newTransaction.receiverAddress = receiverAddress;
+      newTransaction.amount = amount;
+      newTransaction.timestamp = timestamp;
+
+      string signature = this->signTransaction(newTransaction);
+      newTransaction.signature = signature;
+
+      return newTransaction;
+    }
+
+    string signTransaction(Transaction transaction) {
+      return "signature";
+    }
+    void printWallet() {};
 };
 
 int main() {
-  
+  cout << time(0);
 }
