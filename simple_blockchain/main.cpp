2e65080f4f2c6a08cf4d365f3ab164b4319ddfa1
sashh <sashheka@gmail.com>
2023-10-31 21:45:39 +0300

    feat: add constructor initializer list to Block class

--- simple_blockchain/main.cpp
+++ simple_blockchain/main.cpp
@@ -15,7 +15,8 @@ class Transaction {
   public:
     Transaction(const string& senderAddress, const string& receiverAddress, double amount, long timestamp)
         : senderAddress(senderAddress), receiverAddress(receiverAddress), amount(amount), timestamp(timestamp) {}
-
+    
+    string signTransaction(Transaction transaction) {};
     void printTransaction() {
       cout << "Transaction sender address: " << this->senderAddress << "\n";
       cout << "Transaction receiver address: " << this->receiverAddress << "\n";
@@ -32,9 +33,18 @@ class Block {
     string hash;
     string previousHash;
   public:
-    string calculateHash(int& index, long& timestamp, string previousHash) {};
+    Block(int index, long timestamp, int nonce, string& hash, string& previousHash)
+      : index(index), timestamp(timestamp), nonce(nonce), hash(hash), previousHash(previousHash) {}
+
+    string calculateHash(int index, long timestamp, string& previousHash) {};
     int mineBlock(int& difficulty) {};
-    void printBlock() {};
+    void printBlock() {
+      cout << "Block index: " << this->index << "\n";
+      cout << "Block timestamp: " << this->timestamp << "\n";
+      cout << "Block nonce: " << this->nonce << "\n";
+      cout << "Block hash: " << this->hash << "\n";
+      cout << "Block previousHash: " << this->previousHash << "\n";
+    }
 };
 
 class Blockchain {
@@ -42,6 +52,7 @@ class Blockchain {
     vector<Block> chain;
     int difficulty;
   public:
+
     Block createGenesisBlock() {};
     Block addBlock(Block& newBlock) {};
     bool isValid() {};
