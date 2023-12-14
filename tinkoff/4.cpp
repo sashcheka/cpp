b2331df463182f72238fd0dcf0e4be8b2fc50333
sashh <sashheka@gmail.com>
2023-12-14 13:05:57 +0300

    feat: tinkoff 4 task

--- tinkoff/4.cpp
+++ tinkoff/4.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <algorithm>
-#include <string>
-
-bool Comparator(int a, int b) {
-  std::string aStr = std::to_string(a);
-  std::string bStr = std::to_string(b);
-
-  if (aStr.length() != bStr.length()) {
-    return aStr.length() > bStr.length();
-  }
-
-  return a < b;
-}
 
 int main() {
-//  int n, k;
-//  std::cin >> n >> k;
-//  
-//  std::vector<int> nums(n);
-//  int start_sum = 0;
-//  int end_sum = 0;
-//  
-//  for (int i = 0; i < n; i++) {
-//    std::cin >> nums[i];
-//    start_sum += nums[i];
+  int n, k;
+  std::cin >> n >> k;
+  int temp;
+  std::priority_queue<int> max_queue;
+
+  for (int i = 0; i < n; i++) {
+    std::cin >> temp;
+    int weight = 1;
+    while (temp > 0) {
+      int digit = temp % 10;
+      int gain = (9 - digit) * weight;
+      max_queue.push(gain);
+      weight *= 10;
+      temp /= 10;
+    }
+  }
+
+//  while (!max_queue.empty()) {
+//    std::cout << max_queue.top() << std::endl;
+//    max_queue.pop();
 //  }
 
-  std::vector<int> nums = { 5, 32, 120, 530, 840, 69, 1 };
-  
-  std::sort(nums.begin(), nums.end(), Comparator);
-  for (int num : nums) {
-    std::cout << num << " ";
+  long int ans = 0;
+  for (int i = 0; i < k; i++) {
+    if (!max_queue.empty()) {
+      int top = max_queue.top();
+      ans += top;
+      max_queue.pop();
+    }
   }
-  
-//  std::cout << start_sum;
+
+  std::cout << ans;
   
   return EXIT_SUCCESS;
 }
