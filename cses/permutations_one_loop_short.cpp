af1eb7e409bec1e277ea7372d4cab6ba574a956d
sashh <sashheka@gmail.com>
2023-12-24 14:53:45 +0300

    feat: different versions of cses "permutations" task

---
+++ cses/permutations_one_loop_short.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  
+  if (n == 2 || n == 3) {
+    cout << "NO SOLUTION";
+    return 0;
+  }
+  
+  for (int i = 1; i <= n; i++) {
+    (i <= n / 2) ? cout << 2 * i << " " : cout << 2 * (i - n / 2) - 1 << " ";
+  }
+  
+  return 0;
+}
