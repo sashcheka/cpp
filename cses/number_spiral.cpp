32b2af227086c14020b38ceeca7777a8aa8067df
sashh <sashheka@gmail.com>
2023-12-27 22:17:08 +0300

    feat: cses number_spiral task

---
+++ cses/number_spiral.cpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long t;
+  long long y, x;
+  
+  cin >> t;
+  
+  while(t--) {
+    cin >> y >> x;
+
+    if (y > x) {
+      if (y % 2)
+        cout << (y - 1) * (y - 1) + 1 + (x - 1);
+      else
+        cout << y * y - (x - 1);
+    } else {
+      if (x % 2)
+        cout << x * x - (y - 1);
+      else
+        cout << (x - 1) * (x - 1) + 1 + (y - 1);
+    }
+    
+    cout << endl;
+  }
+
+  return 0;
+}
