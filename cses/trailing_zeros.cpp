54871090580ba999130df3c57900049293b878cc
sashh <sashheka@gmail.com>
2023-12-30 12:14:13 +0300

    feat: cses trailing_zeros task

---
+++ cses/trailing_zeros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int ans = 0;
+  
+  for (int i = 5; n / i >= 1; i *= 5) {
+    ans += n / i;
+  }
+  
+  cout << ans;
+
+  return 0;
+}
