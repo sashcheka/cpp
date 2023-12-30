7d523d8acec6144e720414a0313780e9a1eeccfc
sashh <sashheka@gmail.com>
2023-12-30 14:10:16 +0300

    refactor: cses coin_piles_fast task

--- cses/coin_piles_fast.cpp
+++ cses/coin_piles_fast.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-#define j int
-
-inline j g() {
-  j c, n = 0;
+inline int g() {
+  int c, n = 0;
   while(1) {
     c = getchar_unlocked();
     if (c < 48) return n;
@@ -12,11 +10,11 @@ inline j g() {
   return n;
 }
 
-j main() {
-  j t = g();
+int main() {
+  int t = g();
 
   while(t--) {
-    j a = g(), b = g();
+    int a = g(), b = g();
     
     puts(((a + b) % 3 || a * 2 < b || a > b * 2) ? "NO" : "YES");
   }
