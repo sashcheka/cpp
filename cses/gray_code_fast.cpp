bebed5a888a1b56eacf06579964e14513efb1586
sashh <sashheka@gmail.com>
2023-12-31 14:57:20 +0300

    refactor: cses gray_code_fast task

--- cses/gray_code_fast.cpp
+++ cses/gray_code_fast.cpp
@@ -2,28 +2,31 @@
 
 using namespace std;
 
+char gray_code[17];
+
 int main() {
-  char binary[17];
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
   
   int n;
   cin >> n;
   
   int lim = 1<<n;
-  fill(binary, binary + n, '0');
-  binary[n] = '\n';
+  fill(gray_code, gray_code + n, '0');
+  gray_code[n] = '\n';
   
   int curr_num, bit_pos;
   
-  for (int i = 1; i < lim; i++) {
-    cout << binary;
+  for (int i = 1; i <= lim; i++) {
+    cout << gray_code;
     curr_num = i;
     bit_pos = n - 1;
     while (!(curr_num&1)) {
-      curr_num = curr_num>>1;
+      curr_num >>= 1;
       bit_pos--;
     }
     
-    binary[bit_pos] = 1^binary[bit_pos];
+    gray_code[bit_pos] = 97 - gray_code[bit_pos];
       
   }
 }
